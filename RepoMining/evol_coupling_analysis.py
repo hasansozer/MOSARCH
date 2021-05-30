16fd2653bfd2d7b315cf9f2c2cae7c53fd2693dc
Hasan Sözer <hasansozer@hotmail.com>
2021-05-30 23:50:40 +0300

    Update evol_coupling_analysis.py
    refactoring

--- RepoMining/evol_coupling_analysis.py
+++ RepoMining/evol_coupling_analysis.py
@@ -1,18 +1,14 @@
 from pydriller import repository, Repository, ModifiedFile
 from itertools import combinations
 
-source_file_extensions = ['.java', '.c', '.cc', '.cpp', '.h']
+source_file_extensions = ['.java', '.c', '.cc', '.cpp', 'cxx', '.h']
 moduleno = {}
 
-def get_source_files(file_list):
-    return [s for s in file_list if s.filename.endswith(tuple(source_file_extensions))]
-
-def get_full_paths(file_list):
+def get_full_paths_for_Java_source_files(file_list):
     full_paths = []
     for f in file_list:
-        path = f.new_path
-        if path is not None:
-            full_paths.append(path.split('\\', 1)[0] + f.filename.split('.java', 1)[0])
+        if f.new_path is not None:
+            full_paths.append(f.new_path.split('\\', 1)[0] + f.filename.split('.java', 1)[0])
     return full_paths
 
 def extract_modules(committed_file_list):
@@ -48,8 +44,8 @@ committed_files = []
 for commit in repo.traverse_commits():
     files = commit.modified_files
     changed_files = [m for m in commit.modified_files if m.change_type == m.change_type.MODIFY]
-    changed_source_files = get_source_files(changed_files)
-    file_list = get_full_paths(changed_source_files)
+    changed_source_files = [s for s in changed_files if s.filename.endswith(tuple(source_file_extensions))]
+    file_list = get_full_paths_for_Java_source_files(changed_source_files)
     if len(file_list) > 1:
         committed_files.append(file_list)
 

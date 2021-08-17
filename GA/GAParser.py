b389d36a5fd4e058275b88dd03aa6adc6a0f7716
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-08-18 01:11:57 +0300

    Revision, DO NOT MERGE

--- GA/GAParser.py
+++ GA/GAParser.py
@@ -1,17 +1,22 @@
 import sys
 import numpy as np
-np.set_printoptions(threshold=sys.maxsize)
+#np.set_printoptions(threshold=sys.maxsize)
 
-print("Usage python RSFParser.py bash-dependency.rsf bash-clustering.rsf")
+#print("Usage python GAParser.py bash-dependency.rsf bash-clustering.rsf")
 
-def RSFParser(filename):
-	RSFParser.filename = filename
-	RSFParser.clustered_items = []
-	RSFParser.name2ID = {}
-	RSFParser.ID2name = {}
-	RSFParser.total_item_count = 0
-	ParseClusteringInputFile(RSFParser,filename)
-	RSFParser.dsm = [[False for x in range(RSFParser.total_item_count)] for y in range(RSFParser.total_item_count)]
+def GAParser(dependency, clustering):
+	GAParser.dependency = dependency
+	GAParser.clustering = clustering
+	GAParser.clustered_items = []
+	GAParser.name2ID = {}
+	GAParser.ID2name = {}
+	GAParser.total_item_count = 0
+	parse_clustering_input_file(GAParser, GAParser.clustering)
+	GAParser.dsm = [[False for x in range(GAParser.total_item_count)] for y in range(GAParser.total_item_count)]
+	parse_dependency_input_file(GAParser, GAParser.dependency)
+	dsm_np = np.array([elem for row in GAParser.dsm for elem in row])
+	
+	return(dsm_np,GAParser.ID2name, GAParser.clustered_items)
 
 def dependency(parser,e1,e2):
 	sum = 0
@@ -19,7 +24,7 @@ def dependency(parser,e1,e2):
 	if parser.dsm[parser.name2ID.get(e2)][parser.name2ID.get(e1)]: sum+=1
 	return sum
 
-def ParseClusteringInputFile(parser,filename):
+def parse_clustering_input_file(parser,filename):
 	try:
 		f = open(filename, "r+")
 		cluster_name = ""
@@ -43,7 +48,7 @@ def ParseClusteringInputFile(parser,filename):
 			parser.total_item_count += 1
 		f.close()
 	except:
-		print("Error while reading an input file!111")
+		print("Error while reading clustering input file!")
 
 def parse_dependency_input_file(parser, filename):
 	try:
@@ -57,10 +62,4 @@ def parse_dependency_input_file(parser, filename):
 		f.close()
 
 	except:
-		print("Error while reading an input file!222")
-
-RSFParser(sys.argv[2])
-parse_dependency_input_file(RSFParser,sys.argv[1])
-clustered_items = RSFParser.clustered_items
-
-dsm_np = np.array([elem for row in RSFParser.dsm for elem in row])
+		print("Error while reading the dependency input file!")
--- GA/Main.py
+++ GA/Main.py
@@ -9,10 +9,73 @@ import numpy as np
 from GA import GA
 from GAKH import GAKH
 from GAJAYA import GAJAYA
+#from GAParser import GAParser
 import time
 import numpy as np
 np.random.seed(519)
 #%% Input information for Simulation
+
+def GAParser(dependency, clustering):
+	GAParser.dependency = dependency
+	GAParser.clustering = clustering
+	GAParser.clustered_items = []
+	GAParser.name2ID = {}
+	GAParser.ID2name = {}
+	GAParser.total_item_count = 0
+	parse_clustering_input_file(GAParser, GAParser.clustering)
+	GAParser.dsm = [[False for x in range(GAParser.total_item_count)] for y in range(GAParser.total_item_count)]
+	parse_dependency_input_file(GAParser, GAParser.dependency)
+	dsm_np = np.array([elem for row in GAParser.dsm for elem in row])
+	
+	return(dsm_np,GAParser.ID2name, GAParser.clustered_items)
+
+def dependency(parser,e1,e2):
+	sum = 0
+	if parser.dsm[parser.name2ID.get(e1)][parser.name2ID.get(e2)]: sum+=1
+	if parser.dsm[parser.name2ID.get(e2)][parser.name2ID.get(e1)]: sum+=1
+	return sum
+
+def parse_clustering_input_file(parser,filename):
+	try:
+		f = open(filename, "r+")
+		cluster_name = ""
+		current_cluster = ""
+		item_name = ""
+		cluster_count = 0
+		
+		for line in f:
+			tokens = line.split()
+			cluster_name = tokens[1]
+			if current_cluster != cluster_name:
+				current_cluster = cluster_name
+				cluster_count += 1
+				parser.clustered_items.append([])	 
+			
+
+			item_name = tokens[2]
+			parser.clustered_items[cluster_count-1].append(item_name)
+			parser.ID2name.update({parser.total_item_count: item_name })
+			parser.name2ID.update({item_name: parser.total_item_count })
+			parser.total_item_count += 1
+		f.close()
+	except:
+		print("Error while reading clustering input file!")
+
+def parse_dependency_input_file(parser, filename):
+	try:
+		f = open(filename, "r+")
+		
+		for line in f:
+			tokens = line.split()
+			item_name = tokens[1]
+			parser.dsm[parser.name2ID.get(item_name)][parser.name2ID.get(tokens[2])] = True
+
+		f.close()
+
+	except:
+		print("Error while reading the dependency input file!")
+
+
 
 '''GA-related'''
 MaxIts = [10000]                                #Number of iterations
@@ -26,21 +89,20 @@ betas = [0.0008]                              #Rollette wheel ratio
 
 
 '''Problem-related'''
-nClusters = [10]                               #Number of Clusters
-nModules = 500
+#nClusters = [10]                               #Number of Clusters
+#nModules = 500
 
 
 #Esad Burdan bana w_ij ve d_i cekermisin?
 #########################################################################################################################
-w_ij = [list(np.random.randint(0,2,nModules)) for i in range(nModules)]
-d_i = np.zeros(nModules)
-for i in range(nModules):
-    d_i[i] = 0
-    for j in range(nModules):
-        d_i[i] += w_ij[i][j]
+w_ij, d_i, clustered_items = GAParser('bash-dependency.rsf', 'bash-clustering.rsf')
 #########################################################################################################################
+# w_ij : 2D matrix showing the dependencies between files, True or False for each cell in the matrix
+# d_i  : index to name map
+# clustered_items : list of clusters that contain module names, each cluster is also a list
 
-
+nModules = len(w_ij)
+nClusters = [len(clustered_items)]
 
 # =============================================================================
 # name='bash-dependency.rsf'

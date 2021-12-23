eb1a76567d05e1b9a77e4550386ce1b97220074a
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-23 15:13:43 +0300

    Tab bug fixed

--- experiments/2/asdas.py
+++ experiments/2/asdas.py
@@ -57,9 +57,10 @@ parser = RSFParser(clusteringFile)
 parser.parse_dependency_input_file(dependencyFile)
 
 w_ij = np.array(parser.dsm).astype(int)
-d_i = parser.ID2name
-d_ii = copy.deepcopy(parser.name2ID)
+name2id = parser.name2ID
 
+
+# taken from Mahir's code directly
 nModules = len(w_ij)
 d_i = np.zeros(nModules)
 for i in range(nModules):
@@ -68,6 +69,8 @@ for i in range(nModules):
         d_i[i] += w_ij[i][j]
 
 max_num = 0
+
+# find how many clusters there are
 with open(clusteringFile, "r") as f:
     for line in f:
         line = line.strip().split(" ")
@@ -75,12 +78,15 @@ with open(clusteringFile, "r") as f:
         if cluster > max_num:
             max_num = cluster
 
+# initiate the clusters
 modulesOnClusters = [[] for i in range(max_num+1)]
+
+# read the clustering file and put modules on clusters
 with open(clusteringFile, "r") as f:
     for line in f:
         line = line.strip().split(" ")
         cluster = int(line[1])
-        module = d_ii[line[2]]
+        module = name2id[line[2]]
         modulesOnClusters[cluster].append(module)
 
 
@@ -89,7 +95,7 @@ modularity = 0.0
 for l in range(len(modulesOnClusters)):
     for i in modulesOnClusters[l]:
             for j in modulesOnClusters[l]:
-                modularity += w_ij[i][j] - d_i[i]*d_i[j]/ (2*m)
-    modularity = 1/(2*m) * modularity
+                modularity += w_ij[i][j] - d_i[i]*d_i[j] / (2*m)
+modularity = 1/(2*m) * modularity
 
 print(clusteringFile + " Modularity: ", modularity)

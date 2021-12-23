01b0e7fb0816720c99642fc29b2fbff4384c3a6a
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-23 15:23:58 +0300

    Removed clutter

--- experiments/2/asdas.py
+++ experiments/2/asdas.py
@@ -1,30 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  4 17:54:58 2021
-
-@author: Milad
-"""
-#import pandas as pd
 from GA_Parser_Class import *
-import time
 import numpy as np
-import copy
-np.random.seed(519)
-#%% Input information for Simulation
 
-'''GA-related'''
-MaxIts = [10000]                                #Number of iterations
-nPops = [30]                                  #Number of population
-crossProbs = [0.35]                           #Crossover probability
-crossRates = [0.5]                             #Crossover rate  
-muteProbs = [0.7]                             #Mutation probability
-muteRates = [0.06]                            #Mutation Rate
-elitisimProbs = [0.2]                           #Elite Parents Probability
-betas = [0.0005]                              #Rollette wheel ratio
-
-
-'''Problem-related'''                            #Number of Clusters
-#nModules = 500
 
 # =============================================================================
 dependencyFile = "bash-dependency.rsf"
@@ -40,7 +16,6 @@ clusteringFile = "clustered_archstudio-dependency.rsf"
 dependencyFile = "hadoop-dependency.rsf"
 clusteringFile = "clustered_hadoop-dependency.rsf"
 # =============================================================================
-
 
 # =============================================================================
 dependencyFile = "lucene-4.6.1-deps.rsf"
@@ -58,7 +33,7 @@ parser.parse_dependency_input_file(dependencyFile)
 
 w_ij = np.array(parser.dsm).astype(int)
 name2id = parser.name2ID
-
+id2name = parser.ID2name
 
 # taken from Mahir's code directly
 nModules = len(w_ij)
@@ -81,6 +56,7 @@ with open(clusteringFile, "r") as f:
 # initiate the clusters
 modulesOnClusters = [[] for i in range(max_num+1)]
 
+
 # read the clustering file and put modules on clusters
 with open(clusteringFile, "r") as f:
     for line in f:
@@ -96,6 +72,6 @@ for l in range(len(modulesOnClusters)):
     for i in modulesOnClusters[l]:
             for j in modulesOnClusters[l]:
                 modularity += w_ij[i][j] - d_i[i]*d_i[j] / (2*m)
-modularity = 1/(2*m) * modularity
+modularity = 1 / (2 * m) * modularity
 
-print(clusteringFile + " Modularity: ", modularity)
+print("Modularity: ", modularity)

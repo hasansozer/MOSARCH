9f5e2fcefdfed3a3e87b675db2ca0d5ddc54fb69
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2022-03-13 16:51:01 +0300

    Refactor info_extract.py and RSFParser.py

--- dataset/info_extract.py
+++ dataset/info_extract.py
@@ -1,119 +1,46 @@
-total_item_count = 0
-dependency_count = 0
-cluster_count = 0
-clustered_items = []
-name2ID = {}
-dsm = []
 
-def parse_directed_clustering_input_file(filename):
-	global cluster_count
-	global clustered_items
-	global total_item_count
-	global name2ID
-	global dsm
-
-
-	cluster_name = ""
-	current_cluster = ""
-	item_name = ""
-	total_item_count = 0
-	dependency_count = 0
-	cluster_count = 0
-	clustered_items = []
-	name2ID = {}
-	dsm = []
-
-	f = open(filename, "r+")
-	for line in f:
-		tokens = line.split()
-		cluster_name = tokens[1]
-		if current_cluster != cluster_name:
-			current_cluster = cluster_name
-			cluster_count += 1
-			clustered_items.append([])
-
-		item_name = tokens[2]
-		clustered_items[cluster_count - 1].append(item_name)
-		name2ID.update({item_name: total_item_count})
-		total_item_count += 1
-	# for items in clustered_items:
-	#     print(len(items))
-	f.close()
-	# exit()
-	
-
-def parse_directed_dependency_input_file(filename):
-	global total_item_count
-	global dependency_count
-	global name2ID
-	global dsm
-
-	dependency_count = 0
-	dsm = [[0 for i1 in range(total_item_count)] for i2 in range(total_item_count)]
-	a = []
-   
-
-	f = open(filename, "r+")
-	
-	for line in f:
-		tokens = line.split()
-		a.append(tokens)
-		try:
-			dsm[name2ID.get(tokens[1])][name2ID.get(tokens[2])] = 1
-		except:
-			a.pop()
-		dependency_count += 1
-	f.close()
-
-	file = open(filename, "w+")
-
-	for line in a:
-			str1 = "" 
-
-			for ele in line: 
-				str1 += ele
-				str1 += " "
-
-			str1 += "\n"
-			file.write(str1)
-
-	file.close()
-
+from RSFParser import RSFParser
 
 def modularity_directed(dependency_file, clustering_file):
 	
-	global total_item_count
-	global dependency_count
-	global name2ID
-	global dsm
+	print("Modularity_directed dependency file : " , dependency_file)
+	print("Modularity_directed clustering file : " , clustering_file)
 
-	parse_directed_clustering_input_file(clustering_file)
-	parse_directed_dependency_input_file(dependency_file)
-	
+	parser = RSFParser(clustering_file)
+	parser.parse_dependency_input_file(dependency_file)
+	clustered_items = parser.clustered_items
+	cluster_count = len(clustered_items)
+	module_count = parser.total_item_count
+	dependency_count = parser.dependency_count
 
-	d_out = [0 for index in range(total_item_count)]
-	d_in = [0 for index in range(total_item_count)]
-	for x in range(total_item_count):
-		for y in range(total_item_count):
-			d_out[x] += dsm[x][y]
-			d_in[y] += dsm[x][y]
+	d = [0]*module_count
+	d_out = [0 for index in range(module_count)]
+	d_in = [0 for index in range(module_count)]
 
-	total_sum = 0
+	for x in range(module_count):
+		for y in range(module_count):
+			d_out[x] += parser.dsm[x][y]
+			d_in[y] += parser.dsm[x][y]
+
+	total = 0
 	for c in range(cluster_count):
 		modules = clustered_items[c]
 		for e1 in range(len(modules)):
 			for e2 in range(len(modules)):
 				if e1 != e2:
-					total_sum += dsm[name2ID.get(modules[e1])][name2ID.get(modules[e2])]
-					total_sum -= (d_out[name2ID.get(modules[e1])] * d_in[name2ID.get(modules[e2])]) / dependency_count
+					total += parser.dsm[parser.name2ID.get(modules[e1])][parser.name2ID.get(modules[e2])]
+					total -= (d_out[parser.name2ID.get(modules[e1])] * d_in[parser.name2ID.get(modules[e2])]) / dependency_count
 
-	return cluster_count, total_item_count, dependency_count, (1 / dependency_count) * total_sum
+	return cluster_count, module_count, dependency_count, (1 / dependency_count) * total
 
 
 
-from RSFParser import RSFParser
 
 def modularity(dependency_file, clustering_file):
+
+	print("Modularity dependency file : " , dependency_file)
+	print("Modularity clustering file : " , clustering_file)
+
 	parser = RSFParser(clustering_file)
 	parser.parse_dependency_input_file(dependency_file)
 	clustered_items = parser.clustered_items

744b59c14845ac6291b6ad06ba8847e78576b1a0
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-07-08 00:18:52 +0300

    Simple UI

---
+++ GA/ui_mosarch.py
@@ -0,0 +1,93 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+
+import subprocess
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(507, 231)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(170, 40, 191, 31))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(40, 90, 91, 31))
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(40, 130, 91, 31))
+        self.label_2.setAlignment(Qt.AlignCenter)
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(170, 90, 191, 31))
+        self.lineEdit_2 = QLineEdit(self.centralwidget)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(170, 130, 191, 31))
+        self.checkBox = QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(380, 90, 131, 31))
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(40, 40, 91, 31))
+        self.label_3.setAlignment(Qt.AlignCenter)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(230, 180, 75, 24))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+        self.pushButton.clicked.connect(self.runAlgorithm)
+
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"GA", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"GAKH", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"HYGAR", None))
+        self.comboBox.setItemText(3, "")
+
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Dependency File", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Clustering File", None))
+        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"Clean the file", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Algorithm", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"RUN", None))
+
+    def runAlgorithm(self):
+        self.lineEdit.text()
+        output = subprocess.run(['python', 'model.py','--dependency_file', self.lineEdit.text(),'--algorithm', self.comboBox.currentText()], capture_output=True)
+        print(output)
+
+
+app = QApplication(sys.argv)
+window = Ui_MainWindow()
+main_window = QMainWindow()
+window.setupUi(main_window)
+main_window.show()
+sys.exit(app.exec())

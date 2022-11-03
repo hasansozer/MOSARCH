9c7a5ebc8bf9d9b988922950292cbacc5a4ef7cc
Hasan Sözer <hasansozer@hotmail.com>
2022-11-03 22:52:41 +0300

    generate a number of variants for the population based on a provided clustering

--- experiments/mgmc2hygar.py
+++ experiments/mgmc2hygar.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import random
 
 if __name__ == '__main__':
 
@@ -29,21 +30,32 @@ if __name__ == '__main__':
 
     f.close()
 
-    pop_rand = [np.random.random() for _ in range(nModules + nClusters - 1)]
-    pop_rand.sort()
+    nPop = 10
+    population = []
 
-    pop = [0.0 for _ in range(nModules + nClusters - 1)]
+    for p in range(nPop):
+        pop_rand = [np.random.random() for _ in range(nModules + nClusters - 1)]
+        pop_rand.sort()
+        pop = [0.0 for _ in range(nModules + nClusters - 1)]
 
-    cluster_start_index = 0
-    cluster_stop_index = -1
-    for c in range(0, nClusters):
-        cluster_start_index = cluster_stop_index + 1
-        cluster_stop_index = cluster_start_index + len(clustered_items[c])
-        for i in range(len(clustered_items[c])):
-            pop[name2ID[clustered_items[c][i]]] = pop_rand[cluster_start_index + i]
-        if c < nClusters-1:
-            pop[nModules+c] = pop_rand[cluster_stop_index]
+        cluster_ids = np.arange(0, nClusters).tolist()
+        random.shuffle(cluster_ids)
+
+        cluster_start_index = 0
+        cluster_stop_index = -1
+        while cluster_ids:
+            c = cluster_ids.pop()
+            cluster_start_index = cluster_stop_index + 1
+            cluster_stop_index = cluster_start_index + len(clustered_items[c])
+            for i in range(len(clustered_items[c])):
+                pop[name2ID[clustered_items[c][i]]] = pop_rand[cluster_start_index + i]
+            if len(cluster_ids) > 0:
+                pop[nModules+nClusters-len(cluster_ids)-1] = pop_rand[cluster_stop_index]
+        population.append(pop)
 
     print("number of clusters: ", nClusters)
     print("number of modules: ", nModules)
-    print(pop)
+
+    for p in range(nPop):
+        print('population item %d:', p)
+        print(population[p])

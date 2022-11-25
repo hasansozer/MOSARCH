a6c12c6fba159f6741afdd597074af0d1677c83a
hasansozer <hasansozer@hotmail.com>
2022-11-25 11:39:52 +0300

    parsing of the MGMC output is done only once before the main loop

--- GA/MainGAMGMC.py
+++ GA/MainGAMGMC.py
@@ -15,7 +15,7 @@ np.random.seed(519)
 
 '''GA-related'''
 MaxIts = [100000]                                # Number of iterations
-MaxDurations = [9000]                            # Max duration in seconds. Duration has precedence over iterations: If the duration is reached, the algorithm stops even if the iterations are not finished.
+MaxDurations = [36000]                            # Max duration in seconds. Duration has precedence over iterations: If the duration is reached, the algorithm stops even if the iterations are not finished.
 
 nPops = [70]                                  # Number of population
 crossRates = [0.2]                             # Crossover rate  
@@ -54,6 +54,15 @@ dOutArray = [parser.d_out]
 
 outFilePrefix = dependencyFile.split("-")[0]
 
+parser.parse_clustering_input_file(mgmc_clustering_file)
+jaya_list = parser.get_jaya_random_list(mgmc_clustering_file)
+clustered_items = parser.clustered_items
+name2ID = parser.name2ID
+nCluster = len(clustered_items)
+print(str(nCluster))
+outFileSuffix = ""
+isDirected = False
+
 #%% Main Loop
 for MaxIt in MaxIts:
     for MaxDuration in MaxDurations:
@@ -65,20 +74,12 @@ for MaxIt in MaxIts:
                         muteNumber=int(muteProb*nPop)
                         for muteRate in muteRates:
                             for elitismProb in elitisimProbs:
-                                for beta in betas:
-                                    parser.parse_clustering_input_file(mgmc_clustering_file)
-                                    jaya_list = parser.get_jaya_random_list(mgmc_clustering_file)
-                                    clustered_items = parser.clustered_items
-                                    name2ID = parser.name2ID
-                                    nCluster = len(clustered_items)
-                                    outFileSuffix = str(nCluster) + "clusters"
-                                    isDirected = False
+                                for beta in betas:                                                    
                                     inputdata = MaxIt, MaxDuration, nPop, crossNumber, muteNumber, muteRate, elitismProb, beta, nCluster, nModules, w_ij, d_i, crossRate, Dependencies, CodeList, DependencyMatrix, nDependecies, dInArray, dOutArray, isDirected, outFilePrefix + "-" + outFileSuffix 
                                     objectiveGA = 0
                                     cpuGA = 0
                                     objectiveGAKH = 0 
                                     cpuGAKH = 0
-
                                     
                                     q = open(outFilePrefix + "-" + outFileSuffix + "-HYGARIII.csv", "w+")
                                     q.write("Iteration,Iteration_CPU_Time,Total_CPU_Time,Objective\n")

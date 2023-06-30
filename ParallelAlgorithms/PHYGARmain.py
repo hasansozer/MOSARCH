c7fb71c84e4bfc2ab7b52bde655205511971fc40
thvlr <42982677+tahavarol@users.noreply.github.com>
2023-06-30 04:34:31 +0300

    rollout_ready

--- ParallelAlgorithms/PHYGARmain.py
+++ ParallelAlgorithms/PHYGARmain.py
@@ -15,7 +15,7 @@ globals.initialize()
 '''GA-related'''
 
 
-nPops = [32*6]                                  #Number of population
+                                #Number of population
 crossRates = [0.2]                             #Crossover rate  
 muteRates = [0.70]                            #Mutation Rate
 
@@ -31,8 +31,8 @@ globals.patience = 50
 '''Problem-related'''
 nClusters = [5]                               #Number of Clusters
 
-
-software_list = ["bash"]
+nPops = [192,192,128,128,128,128,32,32]
+software_list = ["archstudio", "bash", "hadoop","lucene","nutch","struts2","openjpa","itk"]
 
 algoModes = ["parallel","sequential"]
 
@@ -91,7 +91,7 @@ if runInitialHeuristic:
 
 
 if __name__ == "__main__":
-    for globals.nPop in nPops:
+    for globals.nPop,globals.software in zip(nPops,software_list):
         for crossProb in crossProbs:
             globals.crossNumber = 2*int(crossProb*globals.nPop/2)
             for globals.crossRate in crossRates:
@@ -101,70 +101,69 @@ if __name__ == "__main__":
                         for globals.elitismProb in elitisimProbs:
                             for globals.beta in betas:
                                 for globals.nCluster in nClusters:
-                                    for globals.software in software_list:
-                                        for globals.algoMode in algoModes:
-                                            for algorithm in algorithms:
+                                    for globals.algoMode in algoModes:
+                                        for algorithm in algorithms:
 
-                                                dependencyFile = "dataset/{}/{}-dependency.rsf".format(globals.software,globals.software)
+                                            dependencyFile = "dataset/{}/{}-dependency.rsf".format(globals.software,globals.software)
 
-                                                parser = RSFParser(dependencyFile)
-                                                parser.parse_dependency_input_file(dependencyFile)
+                                            parser = RSFParser(dependencyFile)
+                                            parser.parse_dependency_input_file(dependencyFile)
 
-                                                globals.w_ij = np.array(parser.dsm).astype(np.int16)
+                                            globals.w_ij = np.array(parser.dsm).astype(np.int16)
 
-                                                print("weight matrix sparsity: ",1.0 - (np.count_nonzero(globals.w_ij) / float(globals.w_ij.size)))
-                                                print("weight matrix memory usage: ", round(getsizeof(globals.w_ij) / 1024 / 1024,2))
+                                            print("weight matrix sparsity: ",1.0 - (np.count_nonzero(globals.w_ij) / float(globals.w_ij.size)))
+                                            print("weight matrix memory usage: ", round(getsizeof(globals.w_ij) / 1024 / 1024,2))
 
-                                                globals.d_i = parser.ID2name
-                                                clustered_items = parser.clustered_items
-                                                
+                                            globals.d_i = parser.ID2name
+                                            clustered_items = parser.clustered_items
+                                            
 
-                                                globals.nModules = len(globals.w_ij)
+                                            globals.nModules = len(globals.w_ij)
 
-                                                globals.timeLimit = globals.nModules*5 
+                                            globals.timeLimit = globals.nModules*5 
 
-                                                globals.d_i = np.zeros(globals.nModules)
-                                                for i in range(globals.nModules):
-                                                    globals.d_i[i] = 0
-                                                    for j in range(globals.nModules):
-                                                        globals.d_i[i] += globals.w_ij[i][j]
-                                                        
-                                                globals.m = 0.5 * np.sum(globals.d_i)                                                    
-                                                globals.modMat = np.matmul(globals.d_i.reshape(1,-1).T,globals.d_i.reshape(1,-1))
-                                                globals.modMat = globals.modMat/np.sum(globals.d_i)
-                                                globals.modMat = globals.modMat.astype(np.float16)
-
-                                                print("modularity matrix sparsity: ",1.0 - (np.count_nonzero(globals.modMat) / float(globals.modMat.size)))
-                                                print("modularity graph memory usage: ", round(getsizeof(globals.modMat) / 1024 / 1024,2))
-
-                                                globals.CodeList = list(parser.name2ID.keys())
-                                                globals.DependencyMatrix = [parser.dsm]
-                                                globals.nDependecies = [parser.dependency_count]
-                                                #dInArray = [parser.d_in]
-                                                #dOutArray = [parser.d_out]
-                                                globals.timeLimit = globals.nModules*5 
-                                                print(globals.software, "is being solved", "within time limit of ", globals.timeLimit)
-                                                print(globals.software, "has", globals.nModules, "modules")
-                                                start = time.time()
-
-                                                if runInitialHeuristic:
-                                                    print("Initial clustering algorithm is running....")
-                                                    globals.nCluster = runHeuristics(globals.software)
-                                                    print("Initial clustering algorithm found {} clusters".format(globals.nCluster))
+                                            globals.d_i = np.zeros(globals.nModules)
+                                            for i in range(globals.nModules):
+                                                globals.d_i[i] = 0
+                                                for j in range(globals.nModules):
+                                                    globals.d_i[i] += globals.w_ij[i][j]
                                                     
-                                                if algorithm == "GA":
-                                                    objectiveGAJAYA, clusters = Algorithms.GA()
-                                                    cpuGAJAYA = time.time()-start
-                                                    q=open("{}/Results_{}.csv".format(globals.logging_directory,globals.software), "a+")
-                                                    q.write(globals.algoMode +" "+ algorithm +" "+str(objectiveGAJAYA) + ' ' + str(cpuGAJAYA) + '\n')
-                                                    q.write(str(globals.nPop) + ',' +str(crossProb) + ',' +str(globals.crossRate) + ',' +str(muteProb) + ',' +str(globals.muteRate) + ',' +str(globals.elitismProb) + ',' +str(globals.beta) + ',' + str(objectiveGAJAYA) + ',' + str(cpuGAJAYA) + '\n')
-                                                    q.close()
-                                                if algorithm == "HYGAR":
-                                                    objectiveGAJAYA, clusters = Algorithms.HYGAR()
-                                                    cpuGAJAYA = time.time()-start
-                                                    q=open("{}/Results_{}.csv".format(globals.logging_directory,globals.software), "a+")
-                                                    q.write(globals.algoMode +" "+ algorithm +" "+ str(objectiveGAJAYA) + ' ' + str(cpuGAJAYA) + '\n')
-                                                    q.write(str(globals.nPop) + ',' +str(crossProb) + ',' +str(globals.crossRate) + ',' +str(muteProb) + ',' +str(globals.muteRate) + ',' +str(globals.elitismProb) + ',' +str(globals.beta) + ',' + str(objectiveGAJAYA) + ',' + str(cpuGAJAYA) + '\n')
-                                                    q.close()
+                                            globals.m = 0.5 * np.sum(globals.d_i)                                                    
+                                            globals.modMat = np.matmul(globals.d_i.reshape(1,-1).T,globals.d_i.reshape(1,-1))
+                                            globals.modMat = globals.modMat/np.sum(globals.d_i)
+                                            globals.modMat = globals.modMat.astype(np.float16)
+
+                                            print("modularity matrix sparsity: ",1.0 - (np.count_nonzero(globals.modMat) / float(globals.modMat.size)))
+                                            print("modularity graph memory usage: ", round(getsizeof(globals.modMat) / 1024 / 1024,2))
+
+                                            globals.CodeList = list(parser.name2ID.keys())
+                                            globals.DependencyMatrix = [parser.dsm]
+                                            globals.nDependecies = [parser.dependency_count]
+                                            #dInArray = [parser.d_in]
+                                            #dOutArray = [parser.d_out]
+                                            globals.timeLimit = globals.nModules*5 
+                                            print(globals.software, "is being solved", "within time limit of ", globals.timeLimit)
+                                            print(globals.software, "has", globals.nModules, "modules")
+                                            start = time.time()
+
+                                            if runInitialHeuristic:
+                                                print("Initial clustering algorithm is running....")
+                                                globals.nCluster = runHeuristics(globals.software)
+                                                print("Initial clustering algorithm found {} clusters".format(globals.nCluster))
+                                                
+                                            if algorithm == "GA":
+                                                objectiveGAJAYA, clusters = Algorithms.GA()
+                                                cpuGAJAYA = time.time()-start
+                                                q=open("{}/Results_{}.csv".format(globals.logging_directory,globals.software), "a+")
+                                                q.write(globals.algoMode +" "+ algorithm +" "+str(objectiveGAJAYA) + ' ' + str(cpuGAJAYA) + '\n')
+                                                q.write(str(globals.nPop) + ',' +str(crossProb) + ',' +str(globals.crossRate) + ',' +str(muteProb) + ',' +str(globals.muteRate) + ',' +str(globals.elitismProb) + ',' +str(globals.beta) + ',' + str(objectiveGAJAYA) + ',' + str(cpuGAJAYA) + '\n')
+                                                q.close()
+                                            if algorithm == "HYGAR":
+                                                objectiveGAJAYA, clusters = Algorithms.HYGAR()
+                                                cpuGAJAYA = time.time()-start
+                                                q=open("{}/Results_{}.csv".format(globals.logging_directory,globals.software), "a+")
+                                                q.write(globals.algoMode +" "+ algorithm +" "+ str(objectiveGAJAYA) + ' ' + str(cpuGAJAYA) + '\n')
+                                                q.write(str(globals.nPop) + ',' +str(crossProb) + ',' +str(globals.crossRate) + ',' +str(muteProb) + ',' +str(globals.muteRate) + ',' +str(globals.elitismProb) + ',' +str(globals.beta) + ',' + str(objectiveGAJAYA) + ',' + str(cpuGAJAYA) + '\n')
+                                                q.close()
 
 
